a43dcf1cb90553588dd99d3d34575fe91c63458a
Najim Yaqubie <najimyaqubie@gmail.com>
2019-11-14 03:51:37 -0500

    change table scheme and name

--- src/handlers/get-all-items.js
+++ src/handlers/get-all-items.js
@@ -6,7 +6,7 @@ const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
 // Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+const tableName = process.env.TABLE;
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
